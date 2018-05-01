253bfca9088a3113b87f84582b03dea993a9cd16
Micah Chiang <micahkchiang@gmail.com>
2018-04-30 17:37:54 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <nav>
+        <ul class="nav__left">
+          <li>
+            <router-link to='/'>Home</router-link>
+          </li>
+          <li>
+            <router-link to='/admin'>Admin</router-link>
+          </li>
+          <li>
+            <router-link to='/cart'>Cart</router-link>
+          </li>
+        </ul>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang='scss'>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  background-color: #2c3e50;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+}
+
+.nav__left {
+  list-style: none;
+  display: flex;
+  width: 100%;
+
+  & li {
+    flex: 0 0 10%;
+    & > * {
+      text-decoration: none;
+      color: white;
+    }
+  }
+}
+</style>
---
+++ src/pages/admin/Index.vue
@@ -0,0 +1,39 @@
+<template>
+      <div class="admin__container">
+        <div class="admin__container-menu-container">
+          <ul class="admin__container-menu-container-nav">
+            <!-- Links are to sibling routes -->
+            <li><router-link to="/admin">View Products</router-link></li>
+            <li><router-link to="/admin/new">New Product</router-link></li>
+          </ul>
+        </div>
+        <!-- Outlet for children routes -->
+        <router-view class="router__container"></router-view>
+      </div>
+</template>
+
+<style lang="scss">
+.admin__container {
+  font-size: 1rem;
+  display: flex;
+  margin-top: 2rem;
+  &-menu-container {
+      flex: 0 0 15%;
+      border-right: 1px solid #cccccc;
+      padding: 2rem;
+      &-nav {
+          list-style: none;
+          & li {
+              padding: 1rem 0;
+              font-size: 1.2rem;
+              & > * {
+                  text-decoration: none;
+              }
+          }
+      }
+  }
+  .router__container {
+      flex: 0 0 85%;
+  }
+}
+</style>
